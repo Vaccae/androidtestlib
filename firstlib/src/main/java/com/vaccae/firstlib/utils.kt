585956253e61557e7162231ff2d02ecf8ef12c78
Vaccae <3657447@qq.com>
2020-02-21 11:02:50 +0800

    有

---
+++ firstlib/src/main/java/com/vaccae/firstlib/utils.kt
@@ -0,0 +1,15 @@
+package com.vaccae.firstlib
+
+/**
+ * 作者：Vaccae
+ * 邮箱：3657447@qq.com
+ * 创建时间：2020-02-21 11:01
+ * 功能模块说明：
+ */
+class utils {
+    companion object {
+        fun plus(x: Int, y: Int): Int {
+            return x + y
+        }
+    }
+}
